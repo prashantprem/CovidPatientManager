f8d446614a4f7493bdef5f0c699bb55cc94f1148
Prashant Kumar Upadhyay <56858479+prashantprem@users.noreply.github.com>
2021-07-22 11:09:21 +0530

    Update index.js

--- ExpressJS REST API/index.js
+++ ExpressJS REST API/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 
 const port = process.env.PORT || 5000;
-mongoose.connect("mongodb+srv://admin:prem@$ifsb@cluster0.lircg.mongodb.net/PatientDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://admin:passwordb@cluster0.lircg.mongodb.net/PatientDB?retryWrites=true&w=majority", {
 useNewUrlParser: true,
 useCreateIndex: true,
 useUnifiedTopology: true,});
